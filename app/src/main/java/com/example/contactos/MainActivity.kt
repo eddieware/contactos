86acff324687959f810f264a1f7bdf5365d77d99
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-02-12 10:28:15 -0600

    working intents before butons

--- app/src/main/java/com/example/contactos/MainActivity.kt
+++ app/src/main/java/com/example/contactos/MainActivity.kt
@@ -6,15 +6,83 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var contacto :Int =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Boton contacto1
         imageBtn1.setOnClickListener{
-            //setContentView(R.layout.secondlayout)
+            //setContentView(R.layout.secondlayout) inecesario por la inclusion en el manifesto
             val intent = Intent(this,SecondActivity::class.java)
+            val name = "Rosa Perez"
+            val ocupacion = "Secretaria"
+            val trabaja = "SHCP"
+            val domicilio = "Ocampo No. 234"
+            val ciudad = "Celaya, Gto."
+            val correo = "rosa@gmail.com"
+            val tel = "4611238907"
+
+            intent.putExtra("Name",name)
+            intent.putExtra("Ocupacion",ocupacion)
+            intent.putExtra("Trabaja",trabaja)
+            intent.putExtra("Domicilio",domicilio)
+            intent.putExtra("Ciudad", ciudad)
+            intent.putExtra("Correo",correo)
+            intent.putExtra("Tel",tel)
+
+
             startActivity(intent)
+            finish()
+
+
+        }
+
+        //Boton contacto2
+        imageBtn2.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            val name = "Julián Arreguín"
+            val ocupacion = "Programador"
+            val trabaja = "Apple"
+            val domicilio = "Ocampo No. 235"
+            val ciudad = "Irapuato, Gto."
+            val correo = "julian@gmail.com"
+            val tel = "4611238908"
+
+            intent.putExtra("Name",name)
+            intent.putExtra("Ocupacion",ocupacion)
+            intent.putExtra("Trabaja",trabaja)
+            intent.putExtra("Domicilio",domicilio)
+            intent.putExtra("Ciudad", ciudad)
+            intent.putExtra("Correo",correo)
+            intent.putExtra("Tel",tel)
+
+            startActivity(intent)
+            finish()
+
+        }
+        //Boton3
+        imageBtn3.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            val name = "Susy Ramírez"
+            val ocupacion = "Diseñadora"
+            val trabaja = "Lacoste"
+            val domicilio = "Ocampo No. 236"
+            val ciudad = "Salamanca, Gto."
+            val correo = "susy@gmail.com"
+            val tel = "4611238909"
+
+            intent.putExtra("Name",name)
+            intent.putExtra("Ocupacion",ocupacion)
+            intent.putExtra("Trabaja",trabaja)
+            intent.putExtra("Domicilio",domicilio)
+            intent.putExtra("Ciudad", ciudad)
+            intent.putExtra("Correo",correo)
+            intent.putExtra("Tel",tel)
+
+            startActivity(intent)
+            finish()
 
         }
     }
--- app/src/main/java/com/example/contactos/SecondActivity.kt
+++ app/src/main/java/com/example/contactos/SecondActivity.kt
@@ -1,4 +1,29 @@
 package com.example.contactos
 
-class SecondActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SecondActivity :AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.secondlayout)
+        var intent = intent
+        val name = intent.getStringExtra("Name")
+        val ocupacion = intent.getStringExtra("Ocupacion")
+        val trabaja = intent.getStringExtra("Trabaja")
+        val domicilio = intent.getStringExtra("Domicilio")
+        val ciudad = intent.getStringExtra("Ciudad")
+        val correo = intent.getStringExtra("Correo")
+        val tel = intent.getStringExtra("Tel")
+
+        val resultTv = findViewById<TextView>(R.id.txtName)
+        resultTv.text = name+"\n"+ "Ocupacion: "+ocupacion+"\n"+ "Trabaja en:"+ trabaja+"\n"+
+                "Domicilio: "+domicilio+"\n"+"Ciudad: "+ ciudad +"\n"+"Correo: "+correo+"\n"+"Tel: "+tel
+
+
+
+    }
 }
